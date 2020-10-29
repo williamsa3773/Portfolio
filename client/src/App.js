aadaf3b756746e5c3f003b8cb0b73ad1d754e4e5
williamsa3773 <mMmCheesecake@Andres-MBP.lan>
2020-10-29 01:34:23 -0400

    Forth commit... completed header editing... wokring on containter next

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,16 @@ const App = (props) => {
   return (
     <div className="App">
       <div className="Header">
+        <h1>Andre Giovann Williams</h1>
       </div>
       <div className="Container">
+        <p>Sample Text</p>
       </div>
       <div className="Contact">
+        <p>Sample Text</p>
       </div>
       <div className="Footer">
+        <p>Sample Text</p>
       </div>
     </div>
   )
